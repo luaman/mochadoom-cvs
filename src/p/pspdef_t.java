12755e54fa61b9c6aba23e806c4cd30693d86917
velktron <velktron>
2011-05-05 15:56:31 +0000

    added reader.

--- src/p/pspdef_t.java
+++ src/p/pspdef_t.java
@@ -1,17 +1,29 @@
 package p;
 
+import java.io.IOException;
+
+import w.DoomFile;
+import w.ReadableDoomObject;
 import data.state_t;
 
-public class pspdef_t {
-	
-	public pspdef_t(){
-		state=new state_t();
-	}
-	
-	/** a NULL state means not active */
-	    public state_t	state;	
-	    public int		tics;
-	    /** fixed_t */
-	    public int	sx, sy;
+public class pspdef_t implements ReadableDoomObject{
 
-	}
+    public pspdef_t(){
+        state=new state_t();
+    }
+
+    /** a NULL state means not active */
+    public state_t	state;	
+    public int		tics;
+    /** fixed_t */
+    public int	sx, sy;
+
+    @Override
+    public void read(DoomFile f) throws IOException {
+        state.read(f);
+        tics=f.readInt();
+        sx=f.readInt();
+        sy=f.readInt();
+    }
+
+}
