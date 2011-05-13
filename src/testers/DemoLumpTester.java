4dc3ad5653af839bf62ee340aacc2f8220856cf0
velktron <velktron>
2011-05-13 17:47:39 +0000

    Demo lump tester.

--- src/testers/DemoLumpTester.java
+++ src/testers/DemoLumpTester.java
@@ -1,29 +1,7 @@
 package testers;
 
-import static data.Defines.PU_STATIC;
-import hu.HU;
-
-import java.nio.ByteBuffer;
-
-import m.FixedFloat;
-
-import rr.vertex_t;
-
-import data.mapvertex_t;
-import defines.*;
 import demo.VanillaDoomDemo;
-import doom.DoomContext;
-import doom.DoomStatus;
-
-import utils.C2JUtils;
 import w.*;
-
-/** This is a very simple tester for the WadLoader and HU modules.
- *  We use the same exact methods used in the C source code, only
- *  with a more OO approach.
- * 
- * 
- */
 
 public class DemoLumpTester {
 
@@ -31,7 +9,7 @@ public class DemoLumpTester {
 
     WadLoader W=new WadLoader();
     try {
-		W.InitMultipleFiles(new String[] {"doom1.wad"});
+		W.InitMultipleFiles(new String[] {"C://DOOMS/e1m1.wad"});
 	} catch (Exception e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
