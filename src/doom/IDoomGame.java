6eaa11b0296c316062683b840accda3eb2d39b93
velktron <velktron>
2011-05-20 14:49:50 +0000

    Added more game-specific functions to the interface, away from DoomMain.

--- src/doom/IDoomGame.java
+++ src/doom/IDoomGame.java
@@ -1,5 +1,7 @@
 package doom;
 
+import defines.skill_t;
+
 /** Groups functions formerly in d_game, 
  * in case you want to provide a different implementation 
  */
@@ -8,7 +10,31 @@ public interface IDoomGame {
 
 	 void ExitLevel ();
 	 void WorldDone ();
-     void setGameAction(gameaction_t action);
+
      boolean CheckDemoStatus();
+     
+     /** Can be called by the startup code or M_Responder.
+     A normal game starts at map 1,
+     but a warp test can start elsewhere */
+     
+ public void DeferedInitNew (skill_t skill, int episode, int map);
+     
+     /** Can be called by the startup code or M_Responder,
+     calls P_SetupLevel or W_EnterWorld. */
+   public void LoadGame (String name);
+
+   /** Called by M_Responder. */
+   public void SaveGame (int slot, String description);
+   
+   /** Takes a screenshot *NOW*
+    * 
+    */
+   public void ScreenShot() ;
+   
+   public void StartTitle();
+   
+   public gameaction_t getGameAction();
+
+   public void setGameAction(gameaction_t ga);
 
 }
