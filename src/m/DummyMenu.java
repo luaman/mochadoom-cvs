d259f0086a6d31cff98e37504eb499112a945092
velktron <velktron>
2011-05-24 17:47:15 +0000

    Added Dummy menu for testing.

---
+++ src/m/DummyMenu.java
@@ -0,0 +1,88 @@
+package m;
+
+import v.IVideoScale;
+import doom.DoomStatus;
+import doom.event_t;
+
+public class DummyMenu
+        implements IDoomMenu {
+
+    @Override
+    public void setVideoScale(IVideoScale vs) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void initScaling() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean Responder(event_t ev) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void Ticker() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void Drawer() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void Init() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void StartControlPanel() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean getShowMessages() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void setShowMessages(boolean val) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int getScreenBlocks() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setScreenBlocks(int val) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public int getDetailLevel() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void updateStatus(DoomStatus DS) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
