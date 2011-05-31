ba649974316f8de473008d6484ab0d9ae0a4a549
velktron <velktron>
2011-05-31 17:11:47 +0000

    We're v1.4, baby.

--- src/i/Strings.java
+++ src/i/Strings.java
@@ -1,6 +1,6 @@
 package i;
 
 public final class Strings {
-	public static final String MOCHA_DOOM_TITLE="Mocha Doom Tech Demo v1.31";	
+	public static final String MOCHA_DOOM_TITLE="Mocha Doom Tech Demo v1.4";	
 	
 }
