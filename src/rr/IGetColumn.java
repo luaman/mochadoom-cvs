7df9fdc9f94b8c220c24c6b1982d96206da12693
velktron <velktron>
2011-06-08 12:54:34 +0000

    Forgot to commit this...

--- src/rr/IGetColumn.java
+++ src/rr/IGetColumn.java
@@ -2,7 +2,7 @@ package rr;
 
 public interface IGetColumn {
 
-	byte[] GetColumn(int tex, int col);
-	int getDCSourceOffset();
+	byte[] GetColumn(int tex, int col,int index);
+	int getDCSourceOffset(int index);
 
 }
