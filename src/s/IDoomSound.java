e1ec4c29b02227530d92f65fe8c623322466459b
velktron <velktron>
2011-06-12 20:54:25 +0000

    Streamlined DoomSound into interfacing only with s_sound.c methods, aka NOT system specific.

--- src/s/IDoomSound.java
+++ src/s/IDoomSound.java
@@ -1,5 +1,6 @@
 package s;
 
+import data.sfxinfo_t;
 import data.sounds.musicenum_t;
 import data.sounds.sfxenum_t;
 import p.mobj_t;
@@ -7,7 +8,7 @@ import p.mobj_t;
 // Emacs style mode select   -*- C++ -*- 
 //-----------------------------------------------------------------------------
 //
-// $Id: IDoomSound.java,v 1.2 2011/06/05 22:54:56 velktron Exp $
+// $Id: IDoomSound.java,v 1.3 2011/06/12 20:54:25 velktron Exp $
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 //
@@ -22,11 +23,65 @@ import p.mobj_t;
 // GNU General Public License for more details.
 //
 // DESCRIPTION:
-//	The not so system specific sound interface.
+//	The not so system specific sound interface (s_sound.*)
+// Anything high-level like e.g. handling of panning, sound origin,
+// sound multiplicity etc. should be handled here, but not e.g. actual
+// sound playback, sound threads, etc.
+//  That's the job of ISound and IMusic (roughly equivelnt to i_sound.*, but
+// with separate concerns for SFX and MUSIC.
 //
 //-----------------------------------------------------------------------------
 
 public interface IDoomSound {
+
+	class channel_t{
+
+		    // sound information (if null, channel avail.)
+		    sfxinfo_t	sfxinfo;
+
+		    // origin of sound
+		    mobj_t	origin;
+
+		    // handle of the sound being played
+		    int		handle;
+			}
+	
+	/** Convenience hack */
+	public static final int NUMSFX=sfxenum_t.NUMSFX.ordinal();
+	
+	// Purpose?
+	public static final char snd_prefixen[]
+	                                      = { 'P', 'P', 'A', 'S', 'S', 'S', 'M', 'M', 'M', 'S', 'S', 'S' };
+
+	public static final int S_MAX_VOLUME=127;
+
+	// when to clip out sounds
+	// Does not fit the large outdoor areas.
+	public static final int S_CLIPPING_DIST	=	(1200*0x10000);
+
+	// Distance tp origin when sounds should be maxed out.
+	// This should relate to movement clipping resolution
+	// (see BLOCKMAP handling).
+	// Originally: (200*0x10000).
+	public static final int S_CLOSE_DIST	=(160*0x10000);
+
+	public static final int S_ATTENUATOR	=((S_CLIPPING_DIST-S_CLOSE_DIST)>>m.fixed_t.FRACBITS);
+
+	// Adjustable by menu.
+	//protected final int NORM_VOLUME    		snd_MaxVolume
+
+	public static final int NORM_PITCH    = 		128;
+	public final static int NORM_PRIORITY	=	64;
+	public final static int NORM_SEP		=128;
+
+	public final static int S_PITCH_PERTURB	=	1;
+	public final static int S_STEREO_SWING	=	(96*0x10000);
+
+	// percent attenuation from front to back
+	public final static int S_IFRACVOL	=	30;
+
+	public final static int NA	=		0;
+	public final static int S_NUMCHANNELS=		2;
 
 	/**
 	 * Initializes sound stuff, including volume Sets channels, SFX and music
@@ -48,6 +103,8 @@ public interface IDoomSound {
 
 	/**
 	 * Start sound for thing at <origin> using <sound_id> from sounds.h
+	 * Convenience method using sfxenum_t instead. Delegated to int version.
+	 * 
 	 */
 	public void StartSound(mobj_t origin, sfxenum_t sound_id);
 	
@@ -57,25 +114,14 @@ public interface IDoomSound {
 	/** Stop sound for thing at <origin> */
 	public void StopSound(mobj_t origin);
 
-	/** Start music using <music_id> from sounds.h */
-	public void StartMusic(musicenum_t musicid);
-
 	/**
 	 * Start music using <music_id> from sounds.h, and set whether looping
 	 * 
-	 * @param musicid
+	 * @param musicnum
 	 * @param looping
 	 */
-	public void ChangeMusic(musicenum_t musicid, boolean looping);
+	public void ChangeMusic(int musicnum, boolean looping);
 
-	   /**
-     * Start music using <music_id> from sounds.h, and set whether looping
-     * 
-     * @param musicid
-     * @param looping
-     */
-    public void ChangeMusic(int musicid, boolean looping);
-	
 	/** Stops the music fer sure. */
 	public void StopMusic();
 
@@ -95,11 +141,39 @@ public interface IDoomSound {
 
 	public void SetSfxVolume(int volume);
 
+
+	/** Start music using <music_id> from sounds.h */
 	public void StartMusic(int music_id);
+	
+	/** Start music using <music_id> from sounds.h 
+	 *  Convenience method using musicenum_t.
+	 */
+	public void StartMusic(musicenum_t music_id);
+	
+	//
+	// Internals. 
+	// 
+	// MAES: these appear to be only of value for internal implementation,
+	// and are never called externally. Thus, they might as well
+	// not be part of the interface, even though it's convenient to reuse them.
+	//
+	
+	/*
+	int
+	S_getChannel
+	( mobj_t		origin,
+	  sfxinfo_t	sfxinfo );
 
-	void StopChannel(int channel);
 
-	void ShutdownSound();
+	int
+	S_AdjustSoundParams
+	( mobj_t	listener,
+	  mobj_t	source,
+	  int		vol,
+	  int		sep,
+	  int		pitch );
 
-	void ShutdownMusic();
+	void S_StopChannel(int cnum);
+	*/
+
 }
