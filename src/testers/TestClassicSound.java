1f60bfa03a67cd86af93df9368e56773a12e6d52
velktron <velktron>
2011-07-05 18:37:43 +0000

    Classic sound tester kinda works.

--- src/testers/TestClassicSound.java
+++ src/testers/TestClassicSound.java
@@ -3,6 +3,7 @@ package testers;
 import m.DoomRandom;
 import m.IRandom;
 import s.ClassicDoomSoundDriver;
+import data.sounds.sfxenum_t;
 import doom.DoomStatus;
 import w.WadLoader;
 
@@ -17,14 +18,28 @@ public class TestClassicSound {
 	
 	DS.W=W;
 	DS.RND=RND;
-	W.InitMultipleFiles(new String[]{"doom1.wad"});
+	W.InitMultipleFiles(new String[]{"c:\\iwads\\doom1.wad"});
 	
-	ClassicDoomSoundDriver sound=new ClassicDoomSoundDriver(DS,4);
+	ClassicDoomSoundDriver sound=new ClassicDoomSoundDriver(DS,3);
 	
-	sound.Init(127, 127);
+	sound.InitSound();
+	sound.SetChannels(1);
 	
 	Thread.sleep(1000);
-	sound.StartSoundAtVolume(null, 1, 127);
-	
+	//sound.StartSound(1, 127, 127, 127, 0);
+	for (int i=0;i<350;i++){
+	    
+	    Thread.sleep(1000/35);
+
+	    if (i%35==0) sound.StartSound(sfxenum_t.sfx_plpain.ordinal(), 127, 127, 127-6*i/35, 0);
+	    if (DS.gametic%5!=2){
+	    sound.UpdateSound();
+	    }
+	    sound.SubmitSound();
+	    
+	       DS.gametic++;
 	}
+	sound.ShutdownSound();
+	}
+	
 }
