829c2661058f751fcfad2db1c159de71270fea67
velktron <velktron>
2011-07-16 12:20:38 +0000

    Different testing arrangement.

--- src/testers/TestPatchMaps.java
+++ src/testers/TestPatchMaps.java
@@ -3,29 +3,38 @@ package testers;
 import java.util.HashMap;
 
 import pooling.RoguePatchMap;
+import pooling.RoguePatchMap2;
 
 public class TestPatchMaps {
-    public static final int TESTS=100000;
-    public static final int REPS=10;
+    public static final int TESTS=200000;
+    public static final int REPS=50;
     static byte[][][] stuff=new byte[TESTS][1][1];
     static byte[][][] stuff2=new byte[TESTS][][];
 	// A normal hashmap....
 	static HashMap<Integer,byte[][]> map1= new HashMap<Integer,byte[][]> ();
 	
 	// And a NEW and ENHANCED implementation.
-	static RoguePatchMap map2=new RoguePatchMap();
+	static RoguePatchMap2 map2=new RoguePatchMap2();
 	
     public static void main(String[] argv){
     
     	for (int i=0;i<REPS;i++){
     		System.out.printf("Put %d in Hashmap: %d\n",TESTS,testPutHashmap());
+    	}
+
+    	for (int i=0;i<REPS;i++){
     		System.out.printf("Put %d in Patchmap: %d\n",TESTS,testPutPatchmap());
     	}
+
     	
     	for (int i=0;i<REPS;i++){
     		System.out.printf("Get %d from Hashmap: %d\n",TESTS,testGetHashmap());
+    	}
+
+    	for (int i=0;i<REPS;i++){
     		System.out.printf("Get %d from Patchmap: %d\n",TESTS,testGetPatchmap());
     	}
+
     	
     }
     
