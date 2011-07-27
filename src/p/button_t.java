f6a5fbce285eb3effbe0625a6ea3e38d07dd5e31
velktron <velktron>
2011-07-27 21:27:07 +0000

    Completely implemented button resetting and proper "null" default value.

--- src/p/button_t.java
+++ src/p/button_t.java
@@ -9,10 +9,16 @@ public class button_t implements Resettable{
         public int     btexture;
         public int     btimer;
         public mobj_t soundorg;
+        
+        public button_t(){	
+        	this.btexture=0;
+        	this.btimer=0;
+        	this.where=bwhere_e.top;
+        }
  
         public void reset(){
             this.line=null;
-            this.where=null;
+            this.where=bwhere_e.top;
             this.btexture=0;
             this.btimer=0;
             this.soundorg=null;
