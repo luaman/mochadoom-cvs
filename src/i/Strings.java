973538556b1ebcd13a084dd48b44426fa04cf3ea
velktron <velktron>
2011-08-22 15:28:35 +0000

    Added popup messages

--- src/i/Strings.java
+++ src/i/Strings.java
@@ -3,4 +3,23 @@ package i;
 public final class Strings {
 	public static final String MOCHA_DOOM_TITLE="Mocha Doom Tech Demo Danmaku edition v1.5";	
 	
+	public static final String MODIFIED_GAME=
+		("===========================================================================\n"+
+		 "ATTENTION:  This version of DOOM has been modified.  If you would like to\n"+
+         "get a copy of the original game, call 1-800-IDGAMES or see the readme file.\n"+
+         "        You will not receive technical support for modified games.\n"+
+         "                      press enter to continue\n"+
+         "===========================================================================\n");
+	
+	public static final String MODIFIED_GAME_DIALOG=
+		
+		("<html><center>"+
+		"===========================================================================<br>"+
+		 "ATTENTION:  This version of DOOM has been modified.  If you would like to<br>"+
+         "get a copy of the original game, call 1-800-IDGAMES or see the readme file.<br>"+
+         "        You will not receive technical support for modified games.<br>"+
+         "                      press OK to continue<br>"+
+         "===========================================================================<br>"+
+         "</center></html>");	
+	
 }
