7ff4b07990bc70592235ca656a1e35082571a055
velktron <velktron>
2011-08-23 16:17:48 +0000

    Moved certain level-loader specifics in interface. Later on...who knows.

---
+++ src/p/ILevelLoader.java
@@ -0,0 +1,43 @@
+package p;
+
+public interface ILevelLoader {
+
+	// Lump order in a map WAD: each map needs a couple of lumps
+    // to provide a complete scene geometry description.
+
+    public static final int ML_LABEL = 0;
+
+    /** A separator, name, ExMx or MAPxx */
+    public static final int ML_THINGS = 1;
+
+    /** Monsters, items.. */
+    public static final int ML_LINEDEFS = 2;
+
+    /** LineDefs, from editing */
+    public static final int ML_SIDEDEFS = 3;
+
+    /** SideDefs, from editing */
+    public static final int ML_VERTEXES = 4;
+
+    /** Vertices, edited and BSP splits generated */
+    public static final int ML_SEGS = 5;
+
+    /** LineSegs, from LineDefs split by BSP */
+    public static final int ML_SSECTORS = 6;
+
+    /** SubSectors, list of LineSegs */
+    public static final int ML_NODES = 7;
+
+    /** BSP nodes */
+    public static final int ML_SECTORS = 8;
+
+    /** Sectors, from editing */
+    public static final int ML_REJECT = 9;
+
+    /** LUT, sector-sector visibility */
+    public static final int ML_BLOCKMAP = 10;
+
+
+	
+	
+}
