4f14326913ffae4d1a23820725e58053ed06e01e
velktron <velktron>
2011-09-27 15:56:19 +0000

    fully enabled src field

--- src/w/wadfile_info_t.java
+++ src/w/wadfile_info_t.java
@@ -6,7 +6,7 @@ package w;
 // is no point having these separate entities. This belongs here.
 
 public class wadfile_info_t {
-      String name;
-      wad_source_t src;
-      int handle;
+      public String name;
+      public wad_source_t src;
+      public DoomFile handle;
     }
