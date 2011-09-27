07f0463d7351c6766319274fdbb4b2c8b07319d5
velktron <velktron>
2011-09-27 15:58:35 +0000

    Added x,y constructor.

--- src/s/degenmobj_t.java
+++ src/s/degenmobj_t.java
@@ -1,28 +1,35 @@
 package s;
 
-public final class degenmobj_t implements ISoundOrigin {
+public final class degenmobj_t
+        implements ISoundOrigin {
 
-	private final int x,y, z;
-	
-	public degenmobj_t(int x,int y,int z){
-		this.x=x;
-		this.y=y;
-		this.z=z;
-	}
-	
-	@Override
-	public final int getX() {
-		return x;
-	}
+    private final int x, y, z;
 
-	@Override
-	public final int getY() {
-		return y;
-	}
+    public degenmobj_t(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-	@Override
-	public final int getZ() {
-		return z;
-	}
-	
+    public degenmobj_t(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.z = 0;
+    }
+
+    @Override
+    public final int getX() {
+        return x;
+    }
+
+    @Override
+    public final int getY() {
+        return y;
+    }
+
+    @Override
+    public final int getZ() {
+        return z;
+    }
+
 }
