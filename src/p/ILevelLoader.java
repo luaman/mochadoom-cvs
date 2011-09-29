fd57f249b529abf7b8d8622e02dacb96f9da47a5
velktron <velktron>
2011-09-29 13:26:31 +0000

    Expanded to contain externally callable methods.

--- src/p/ILevelLoader.java
+++ src/p/ILevelLoader.java
@@ -1,6 +1,10 @@
 package p;
 
-public interface ILevelLoader {
+import i.DoomStatusAware;
+import rr.subsector_t;
+import defines.skill_t;
+
+public interface ILevelLoader extends DoomStatusAware {
 
 	// Lump order in a map WAD: each map needs a couple of lumps
     // to provide a complete scene geometry description.
@@ -36,11 +40,44 @@ public interface ILevelLoader {
 
     /** LUT, sector-sector visibility */
     public static final int ML_BLOCKMAP = 10;
+    
+    // Maintain single and multi player starting spots.
+    public static final int MAX_DEATHMATCH_STARTS  = 10;
 
     /** Expected lump names for verification */
     public static final String[] LABELS={"MAPNAME","THINGS","LINEDEFS","SIDEDEFS",
                                         "VERTEXES","SEGS","SSECTORS","NODES",
                                         "SECTORS","REJECT","BLOCKMAP"};
+
+    /** P_SetupLevel 
+     * 
+     * @param episode
+     * @param map
+     * @param playermask
+     * @param skill
+     */
+	void SetupLevel(int episode, int map, int playermask, skill_t skill);
+
+	/**
+	 * P_SetThingPosition Links a thing into both a block and a subsector based
+	 * on it's x y. Sets thing.subsector properly
+	 *
+	 * 
+	 * @param thing
+	 */
+	void SetThingPosition(mobj_t thing);
+
+	  /**
+	   * R_PointInSubsector
+	   * 
+	   * MAES: it makes more sense to have this here.
+	   * 
+	   * @param x fixed
+	   * @param y fixed
+	   * 
+	   */
+	
+	subsector_t PointInSubsector(int x, int y);
         
 	
 }
