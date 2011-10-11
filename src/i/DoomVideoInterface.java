d662ad858fdd040213b3ba47933907d8b9b01eaf
velktron <velktron>
2011-10-11 13:25:32 +0000

    Interface changes.

--- src/i/DoomVideoInterface.java
+++ src/i/DoomVideoInterface.java
@@ -1,6 +1,17 @@
 package i;
 
-/** Methods specific to Doom-System video interfacing */
+/** Methods specific to Doom-System video interfacing. 
+ *  In essence, whatever you are using as a final system-specific way to display
+ *  the screens, should be able to respond to these commands. In particular,
+ *  screen update requests must be honored, and palette/gamma request changes
+ *  must be intercepted before they are forwarded to the renderers (in case they
+ *  are system-specific, rather than renderer-specific).
+ *  
+ *  The idea is that the final screen rendering module sees/handles as less as
+ *  possible, and only gets a screen to render, no matter what depth it is.
+ * 
+ *  
+ */
 
 public interface DoomVideoInterface {
 
@@ -18,7 +29,7 @@ public interface DoomVideoInterface {
 
 	public void ShutdownGraphics();
 
-	public void setGamma(int usegamma);
+	public void SetGamma(int gammalevel);
 	
 	public void ReadScreen(byte[] linear);
 
