baee749724e5f95652b5337fd779653629e0a962
velktron <velktron>
2011-10-25 19:41:18 +0000

    Support for lump and file sizes up to 4 GB (at least on paper)

--- src/w/filelump_t.java
+++ src/w/filelump_t.java
@@ -14,7 +14,7 @@ import m.Swap;
  */
 
 public class filelump_t  implements IReadableDoomObject {
-        int         filepos;
+        long         filepos;
         long         size; // Is INT 32-bit in file!
         String        name;
         
@@ -22,18 +22,12 @@ public class filelump_t  implements IReadableDoomObject {
             // MAES: Byte Buffers actually make it convenient changing byte order on-the-fly.
             // But RandomAccessFiles don't :-S
 
-            filepos=DoomIO.readLEInt(f);
-            size=DoomIO.readLEInt(f);
+            filepos=DoomIO.readUnsignedLEInt(f);
+            size=DoomIO.readUnsignedLEInt(f);
             // Names used in the reading subsystem should be upper case.
             name=DoomIO.readNullTerminatedString(f,8).toUpperCase();
         }
 
-        public void write(DataOutputStream f) throws IOException{
-            f.writeInt(filepos);
-            f.write((int)this.size);
-            DoomIO.writeString(f,this.name,8);
-        }
-        
         public static int sizeof(){
             return (4+4+8);
         }
