7dad78b1ad13c79697054812947b7cf8e44fb519
velktron <velktron>
2011-11-01 11:22:34 +0000

    Cleaned up, made flags into final constants.

--- src/p/mobj_t.java
+++ src/p/mobj_t.java
@@ -27,7 +27,6 @@ import data.spritenum_t;
 import data.state_t;
 import defines.*;
 import doom.player_t;
-import doom.think_t;
 import doom.thinker_t;
 
 /**
@@ -192,88 +191,88 @@ public class mobj_t extends thinker_t implements ISoundOrigin, Interceptable,
 	// // MF_ flags for mobjs.
 
 	// Call P_SpecialThing when touched.
-	public static int MF_SPECIAL = 1;
+	public static final int MF_SPECIAL = 1;
 	// Blocks.
-	public static int MF_SOLID = 2;
+	public static final int MF_SOLID = 2;
 	// Can be hit.
-	public static int MF_SHOOTABLE = 4;
+	public static final int MF_SHOOTABLE = 4;
 	// Don't use the sector links (invisible but touchable).
-	public static int MF_NOSECTOR = 8;
+	public static final int MF_NOSECTOR = 8;
 	// Don't use the blocklinks (inert but displayable)
-	public static int MF_NOBLOCKMAP = 16;
+	public static final int MF_NOBLOCKMAP = 16;
 
 	// Not to be activated by sound, deaf monster.
-	public static int MF_AMBUSH = 32;
+	public static final int MF_AMBUSH = 32;
 	// Will try to attack right back.
-	public static int MF_JUSTHIT = 64;
+	public static final int MF_JUSTHIT = 64;
 	// Will take at least one step before attacking.
-	public static int MF_JUSTATTACKED = 128;
+	public static final int MF_JUSTATTACKED = 128;
 	// On level spawning (initial position),
 	// hang from ceiling instead of stand on floor.
-	public static int MF_SPAWNCEILING = 256;
+	public static final int MF_SPAWNCEILING = 256;
 	// Don't apply gravity (every tic),
 	// that is, object will float, keeping current height
 	// or changing it actively.
-	public static int MF_NOGRAVITY = 512;
+	public static final int MF_NOGRAVITY = 512;
 
 	// Movement flags.
 	// This allows jumps from high places.
-	public static int MF_DROPOFF = 0x400;
+	public static final int MF_DROPOFF = 0x400;
 	// For players, will pick up items.
-	public static int MF_PICKUP = 0x800;
+	public static final int MF_PICKUP = 0x800;
 	// Player cheat. ???
-	public static int MF_NOCLIP = 0x1000;
+	public static final int MF_NOCLIP = 0x1000;
 	// Player: keep info about sliding along walls.
-	public static int MF_SLIDE = 0x2000;
+	public static final int MF_SLIDE = 0x2000;
 	// Allow moves to any height, no gravity.
 	// For active floaters, e.g. cacodemons, pain elementals.
-	public static int MF_FLOAT = 0x4000;
+	public static final int MF_FLOAT = 0x4000;
 	// Don't cross lines
 	// ??? or look at heights on teleport.
-	public static int MF_TELEPORT = 0x8000;
+	public static final int MF_TELEPORT = 0x8000;
 	// Don't hit same species, explode on block.
 	// Player missiles as well as fireballs of various kinds.
-	public static int MF_MISSILE = 0x10000;
+	public static final int MF_MISSILE = 0x10000;
 	// Dropped by a demon, not level spawned.
 	// E.g. ammo clips dropped by dying former humans.
-	public static int MF_DROPPED = 0x20000;
+	public static final int MF_DROPPED = 0x20000;
 	// Use fuzzy draw (shadow demons or spectres),
 	// temporary player invisibility powerup.
-	public static int MF_SHADOW = 0x40000;
+	public static final int MF_SHADOW = 0x40000;
 	// Flag: don't bleed when shot (use puff),
 	// barrels and shootable furniture shall not bleed.
-	public static int MF_NOBLOOD = 0x80000;
+	public static final int MF_NOBLOOD = 0x80000;
 	// Don't stop moving halfway off a step,
 	// that is, have dead bodies slide down all the way.
-	public static int MF_CORPSE = 0x100000;
+	public static final int MF_CORPSE = 0x100000;
 	// Floating to a height for a move, ???
 	// don't auto float to target's height.
-	public static int MF_INFLOAT = 0x200000;
+	public static final int MF_INFLOAT = 0x200000;
 
 	// On kill, count this enemy object
 	// towards intermission kill total.
 	// Happy gathering.
-	public static int MF_COUNTKILL = 0x400000;
+	public static final int MF_COUNTKILL = 0x400000;
 
 	// On picking up, count this item object
 	// towards intermission item total.
-	public static int MF_COUNTITEM = 0x800000;
+	public static final int MF_COUNTITEM = 0x800000;
 
 	// Special handling: skull in flight.
 	// Neither a cacodemon nor a missile.
-	public static int MF_SKULLFLY = 0x1000000;
+	public static final int MF_SKULLFLY = 0x1000000;
 
 	// Don't spawn this object
 	// in death match mode (e.g. key cards).
-	public static int MF_NOTDMATCH = 0x2000000;
+	public static final int MF_NOTDMATCH = 0x2000000;
 
 	// Player sprites in multiplayer modes are modified
 	// using an internal color lookup table for re-indexing.
 	// If 0x4 0x8 or 0xc,
 	// use a translation table for player colormaps
-	public static int MF_TRANSLATION = 0xc000000;
+	public static final int MF_TRANSLATION = 0xc000000;
 	// Hmm ???.
-	public static int MF_TRANSSHIFT = 26;
+	public static final int MF_TRANSSHIFT = 26;
 
 	/*
 	 * The following methods were for the most part "contextless" and
