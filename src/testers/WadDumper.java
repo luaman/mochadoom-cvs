f2b2d34d6a17325a8d44d56c6158eb07aab042ad
velktron <velktron>
2011-11-12 22:51:09 +0000

    Made it MOAR awesome. And on a Pentium III!

--- src/testers/WadDumper.java
+++ src/testers/WadDumper.java
@@ -1,29 +1,11 @@
 package testers;
 
-import static data.Defines.PU_STATIC;
-import hu.HU;
-
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.nio.ByteBuffer;
-
-import m.FixedFloat;
-
-import rr.vertex_t;
-
-import data.mapvertex_t;
-import defines.*;
-import doom.DoomContext;
-import doom.DoomStatus;
-
-import utils.C2JUtils;
 import w.*;
 
-/** This is a very simple tester for the WadLoader and HU modules.
- *  We use the same exact methods used in the C source code, only
- *  with a more OO approach.
- * 
+/** DUMPS a WAD's LUMPS to disk. Any questions?! 
  * 
  */
 
@@ -32,7 +14,7 @@ public class WadDumper {
     public static void main(String[] argv) {
         try {
     IWadLoader W=new WadLoader();
-    W.InitMultipleFiles(new String[] {"doom1.wad"});
+    W.InitMultipleFiles(argv);
     BufferedOutputStream bos;
     
     int numlumps=W.NumLumps();
