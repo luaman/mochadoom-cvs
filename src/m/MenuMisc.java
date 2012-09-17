dc56338915231750d80458a0a96d90666ff8c1ad
velktron <velktron>
2012-09-17 15:58:58 +0000

    Defaults loading & handling moved out to variables management subsystem

--- src/m/MenuMisc.java
+++ src/m/MenuMisc.java
@@ -3,37 +3,25 @@ package m;
 import i.DoomSystem;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferShort;
 import java.awt.image.DataBufferUShort;
-import java.awt.image.Raster;
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.io.PrintStream;
-import java.io.RandomAccessFile;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
-import java.util.StringTokenizer;
-
 import javax.imageio.ImageIO;
 
-import doom.DoomStatus;
-import utils.C2JUtils;
 import w.IWritableDoomObject;
 
 // Emacs style mode select   -*- C++ -*- 
 //-----------------------------------------------------------------------------
 //
-// $Id: MenuMisc.java,v 1.28.2.2 2011/11/18 21:37:59 velktron Exp $
+// $Id: MenuMisc.java,v 1.28.2.3 2012/09/17 15:58:58 velktron Exp $
 //
 // Copyright (C) 1993-1996 by id Software, Inc.
 //
@@ -57,189 +45,8 @@ import w.IWritableDoomObject;
 
 public abstract class MenuMisc{
 
-    public static final String rcsid = "$Id: MenuMisc.java,v 1.28.2.2 2011/11/18 21:37:59 velktron Exp $";
-
-    public static String defaultfile;
-    public static String basedefault="default.cfg";
-
-    //
-    // M_SaveDefaults
-    //
-    public static void SaveDefaults (DoomStatus DS)
-    {
-        int     i;
-        int     v;
-        OutputStream f;
-        PrintStream  ps;
-
-        try {
-            f = new FileOutputStream(defaultfile);
-        } catch (FileNotFoundException e) {
-            // can't write the file, but don't complain
-            return;
-        }
-
-        ps=new PrintStream(f);
-        for (Settings set :Settings.values())
-        {
-            if (set.defaultvalue > -0xfff
-                    && set.defaultvalue < 0xfff)
-            {
-
-                if (set.numeric)
-                    ps.printf("%s\t\t%d\n",set.name(),set.setvalue);
-                else
-                    ps.printf("%s\t\t\"%s\"\n",set.name(),set.setszvalue);             
-                //* TODO: will need another big switch thing to 
-                // properly save CURRENT settings to the file.
-            }
-        }
-        try {
-            f.close();}
-        catch (IOException e) {
-            // Well duh....
-            return;
-        }
-    }
-
-    //
-    // M_LoadDefaults
-    //
-
-    public static void LoadDefaults (DoomStatus DS)
-    {
-        int		i;
-        BufferedReader	in;
-        boolean	isstring;
-
-        // set everything to base values
-        int numdefaults = Settings.values().length;
-        for (i=0 ; i<numdefaults ; i++)
-            Settings.values()[i].applySetting(DS);
-
-        // check for a custom default file
-        i = DS.CM.CheckParm("-config");
-        if ((i>0) && i<DS.CM.getArgc()-1)
-        {
-            defaultfile = DS.CM.getArgv(i+1);
-            //System.out.printf("	default file: %s\n",defaultfile);
-        }
-        else
-            defaultfile = basedefault;
-
-        try {
-
-            // read the file in, overriding any set defaults
-            in = new BufferedReader(new InputStreamReader(new BufferedInputStream(new FileInputStream(defaultfile))));
-            if (in!=null)
-            {
-                String name = null, value = null;
-                String s;
-                
-                // Let's make this the sane way...read a string.
-                while (( s=in.readLine())!=null)
-                {
-
-                    isstring = false;
-                    StringTokenizer tk=new StringTokenizer(s);
-
-                    // They should be exactly two.
-                    int tokens=tk.countTokens();
-                    if (tokens==2){
-                        name=tk.nextToken();
-                        value=tk.nextToken();
-                        String tmp=C2JUtils.unquote(value,'"');
-                        if (tmp!=null) value=tmp;
-                    } else if (tokens>2){
-                        // String tokenizer can't recognize quoted strings
-                        name=tk.nextToken();
-                        value = C2JUtils.unquote(s,'"');
-                        if (value==null) continue;
-
-                    }
-
-                        // All settings should be lower case.
-                        name=name.toLowerCase();
-                        //System.out.printf("NAME: %s VALUE: %s\n",name,value);
-                        Settings setme;
-                        try {
-                            setme=Enum.valueOf(Settings.class,name);
-                        } catch (IllegalArgumentException e){
-                            // Not a known type, nothing to set.
-                            System.err.printf("Unknown setting: %s\n",name);
-                            // Next while.
-                            continue;
-                        }
-
-                        // OK, so it's a known setting. Value?
-
-                        if (value.charAt(0)=='"'){
-
-                            // get a string default
-                            setme.setszvalue=value;
-                            if (!setme.numeric)
-                                setme.applySetting(DS);
-                        } else {
-                            
-                            boolean fail=false;
-                            try {
-                                // Try it as an integer.
-                                setme.setvalue= Integer.parseInt(value);
-                            } catch (NumberFormatException e){
-                                fail=true;
-                            }
-                            
-                            if (fail)
-                             try {    
-                                  // Try it as a hex value. Must be preceded by 0x
-                                 fail=false;
-                                 int hexpos=value.toLowerCase().indexOf("0x");
-                                 if (hexpos>-1){
-                                     String substring=value.substring(hexpos+2);
-                                     setme.setvalue= Integer.parseInt(substring,16);
-                                 } else 
-                                 fail=true;
-                             } catch (NumberFormatException e){
-                                 fail=true;
-                             }
-                             
-                             if (fail)
-                                 try {
-                                      // Try it as a character. Must be quoted for
-                                     // this to work, with a length of 3.
-                                     fail=false;
-                                     //System.out.printf("Trying %s\n",value);
-                                     if (value.length()==3 && 
-                                         value.charAt(0)=='\'' &&
-                                         value.charAt(2)=='\'') {
-                                      setme.setvalue= value.charAt(1);
-                                     //System.out.printf("%s set to value %d or character %c\n",setme.name(),setme.setvalue,value.charAt(1));
-                                     } else continue;
-                                 } catch (NumberFormatException e){
-                                     fail=true;
-                                        }
-                                 
-                                 if (fail) {
-                                System.err.printf("Cannot parse expected numerical setting: %s of %s\n",value,name);
-                                // Next while.
-                                continue;
-                                 }
-                            }
-                            if (setme.numeric){
-                                
-                                setme.applySetting(DS);
-                            }
-                } // end-while
-
-
-                in.close();
-            } // not null
-        }catch (IOException e){
-            System.err.printf("I just can't read the settings file %s, will use defaults.\n",defaultfile);
-        }
-    }
-
-
+    public static final String rcsid = "$Id: MenuMisc.java,v 1.28.2.3 2012/09/17 15:58:58 velktron Exp $";
+  
     //
     // SCREEN SHOTS
     //
@@ -280,14 +87,14 @@ public abstract class MenuMisc{
      */
     public static ByteBuffer ReadFile(String name) {
         BufferedInputStream handle;
-        int count, length;
+        int length;
         // struct stat fileinfo;
         ByteBuffer buf;
         try {
             handle = new BufferedInputStream(new FileInputStream(name));
             length = (int) handle.available();
             buf = ByteBuffer.allocate(length);
-            count = handle.read(buf.array());
+            handle.read(buf.array());
             handle.close();
         } catch (Exception e) {
             DoomSystem.MiscError("Couldn't read file %s (%s)", name, e.getMessage());
@@ -333,7 +140,6 @@ public abstract class MenuMisc{
    int       height,
    byte[]     palette )
  {
-     int     i;
      int     length;
      pcx_t  pcx;
      byte[]   pack;
@@ -361,9 +167,8 @@ public abstract class MenuMisc{
      // pack the image
      //pack = &pcx->data;
      int p_pack=0;
-     int p_data=0;
      
-     for (i=0 ; i<width*height ; i++)
+     for (int i=0 ; i<width*height ; i++)
      {
      if ( (data[i] & 0xc0) != 0xc0)
          pack[p_pack++] = data[i];
@@ -376,7 +181,7 @@ public abstract class MenuMisc{
      
      // write the palette
      pack[p_pack++] = 0x0c; // palette ID byte
-     for (i=0 ; i<768 ; i++)
+     for (int i=0 ; i<768 ; i++)
          pack[p_pack++] = palette[i];
      
      // write output file
@@ -426,6 +231,9 @@ public abstract class MenuMisc{
 }
 
 // $Log: MenuMisc.java,v $
+// Revision 1.28.2.3  2012/09/17 15:58:58  velktron
+// Defaults loading & handling moved out to variables management subsystem
+//
 // Revision 1.28.2.2  2011/11/18 21:37:59  velktron
 // Saves PNGs now.
 //
--- src/m/Settings.java
+++ src/m/Settings.java
@@ -2,23 +2,20 @@ package m;
 
 import static g.Keys.*;
 import static doom.englsh.*;
-import utils.C2JUtils;
-import doom.DoomStatus;
 
 
-/** An anumeration with all possible settings and their defaults 
- *  Given Doom's status as a parameter, they can "autoapply" themselves.
- *  The can also be modified by reading from a file, or be used to
- *  generate a string to be written to a file.
+/** An anumeration with the most basic default Doom settings their default
+ *  values, used if nothing else is available. They are applied first thing, 
+ *  and then updated from the .cfg file. 
  *  
  */
 
 public enum Settings {
-    mouse_sensitivity(5),
-    sfx_volume(8),
-   music_volume(8),
-   show_messages(1),
-   alwaysrun(1),
+    mouse_sensitivity("5"),
+    sfx_volume("8"),
+   music_volume("8"),
+   show_messages("1"),
+   alwaysrun("1"),
    key_right(KEY_RIGHTARROW),
    key_left(KEY_LEFTARROW),
    key_up('w'),
@@ -54,177 +51,14 @@ public enum Settings {
    chatmacro8(HUSTR_CHATMACRO8 ),
    chatmacro9( HUSTR_CHATMACRO9 );
 
-        private Settings(int defaultval){
-            this.setvalue=defaultval;
-            this.defaultvalue=defaultval;
-            this.numeric=true;
-            this.szdefaultval=Integer.toString(defaultvalue);
+         private Settings(String defaultval){
+            this.value=defaultval;
         }
+         
 
-private Settings(String defaultval){
-    this.defaultvalue=-1;
-    this.setszvalue=new String(defaultval);;
-    this.szdefaultval=new String(defaultval);
-    this.numeric=false;
-}
-
-final static char w='w';
-final static char a='a';
-final static char s='s';
-final static char d='d';
-
-/** Each setting "knows" where it is appliable. Hardcoded,
- *  mega-switch statement hack, unless you want to use reflection (ugh...)
- *  
- * @param DS
- */
-public void applySetting(DoomStatus DS){
+         private Settings(int defaultval){
+            this.value=Integer.toString(defaultval);
+        }
     
-    int val=0;
-    String Stringval;
-    if (this.numeric)
-        val=this.setvalue;
-    
-    if (this.numeric){
-   switch(this){   
-   case sfx_volume:
-       DS.snd_SfxVolume=val;
-       break;
-   case music_volume:
-       DS.snd_MusicVolume=val;
-       break;
-   case show_messages: 
-       DS.M.setShowMessages(C2JUtils.eval(val));
-       break;
-   case alwaysrun:
-       DS.alwaysrun=C2JUtils.eval(val);
-       break;      
-   case key_right:
-       DS.key_right=val;
-       break;
-   case key_left:
-       DS.key_left=val;
-       break;
-   case key_up:
-       DS.key_up=val;
-       break;
-   case key_down:
-       DS.key_down=val;
-       break;
-   case key_strafeleft:
-       DS.key_strafeleft=val;
-       break;
-   case key_straferight:
-       DS.key_straferight=val;
-       break;
-   case key_fire:
-       DS.key_fire=val;
-       break;
-   case key_use:
-       DS.key_use=val;
-       break;
-   case key_strafe:
-       DS.key_strafe=val;
-       break;
-   case key_speed:
-       DS.key_speed=val;
-       break;
-   case use_mouse:
-       DS.use_mouse=C2JUtils.eval(val);
-       break;
-   case mouseb_fire:
-       //DS.mouseb_fire=val;
-       break;
-   case mouseb_strafe:
-       //DS.mouseb_fire=val;
-       break;
-   case mouseb_forward:
-       //DS.mouseb_fire=val;
-       break;   
-   case use_joystick:
-       //DS.mouseb_fire=val;
-       break;
-   case joyb_fire:
-       //DS.mouseb_fire=val;
-       break;
-   case joyb_strafe:
-       //DS.mouseb_fire=val;
-       break;   
-   case joyb_use:
-       //DS.mouseb_fire=val;
-       break;
-   case joyb_speed:
-       //DS.mouseb_fire=val;
-       break;
-   case screenblocks:
-       DS.M.setScreenBlocks(val);
-       break;
-   case detaillevel:
-       //DS.mouseb_fire=val;
-       break;   
-   case snd_channels:
-       // Defined in sound.c, originally.
-       DS.numChannels=val;
-       break;
-   case mb_used:
-       // Memory used. Any practical use?
-       //TODO DS.mb_used=val;
-       break;
-   } // end mega-switch
-    } // end-numeric
-   else {
-       switch (this){
-       case chatmacro0:
-           DS.HU.setChatMacro(0, setszvalue);
-           break;
-       case chatmacro1:
-           DS.HU.setChatMacro(1, setszvalue);
-           break;
-       case chatmacro2:
-           DS.HU.setChatMacro(2, setszvalue);
-           break;
-       case chatmacro3:
-           DS.HU.setChatMacro(3, setszvalue);
-           break;
-       case chatmacro4:
-           DS.HU.setChatMacro(4, setszvalue);
-           break;
-       case chatmacro5:
-           DS.HU.setChatMacro(5, setszvalue);
-           break;
-       case chatmacro6:
-           DS.HU.setChatMacro(6, setszvalue);
-           break;
-       case chatmacro7:
-           DS.HU.setChatMacro(7, setszvalue);
-           break;
-       case chatmacro8:
-           DS.HU.setChatMacro(8, setszvalue);
-           break;
-       case chatmacro9:
-           DS.HU.setChatMacro(9, setszvalue);
-           break;
-           }
-    } // end string
+    public String value;
 }
-   
-   
-
-
-// UNIX hack, to be removed. 
-//#ifdef SNDSERV
-//    new default_t("sndserver", (int *) &sndserver_filename, (int) "sndserver"),    
-//#endif    
-//#endif
-
-//#ifdef LINUX
-//    new default_t("mousedev", (int*)&mousedev, (int)"/dev/ttyS0"),
-//    new default_t("mousetype", (int*)&mousetype, (int)"microsoft"),
-//#endif
-
-        public int setvalue;
-        public String setszvalue;
-        public final int defaultvalue;
-        public final String szdefaultval;
-        public boolean numeric=false;
-    }
