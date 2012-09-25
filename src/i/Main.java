f45fd7a0425b1c7b1af65188c39df2c1b9fa0133
velktron <velktron>
2012-09-25 16:35:38 +0000

    Added fugly static initializer.

--- src/i/Main.java
+++ src/i/Main.java
@@ -2,6 +2,8 @@ package i;
 
 import java.io.IOException;
 
+import rr.LightsAndColors;
+
 import v.VideoScaleInfo;
 import doom.CommandLine;
 import doom.DoomMain;
@@ -10,7 +12,7 @@ import doom.ICommandLineManager;
 //Emacs style mode select   -*- C++ -*- 
 //-----------------------------------------------------------------------------
 //
-//$Id: Main.java,v 1.11 2012/09/24 17:16:22 velktron Exp $
+//$Id: Main.java,v 1.12 2012/09/25 16:35:38 velktron Exp $
 //
 //Copyright (C) 1993-1996 by id Software, Inc.
 //
@@ -24,52 +26,6 @@ import doom.ICommandLineManager;
 //MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //GNU General Public License for more details.
 //
-//$Log: Main.java,v $
-//Revision 1.11  2012/09/24 17:16:22  velktron
-//Massive merge between HiColor and HEAD. There's no difference from now on, and development continues on HEAD.
-//
-//Revision 1.10.2.2  2012/09/24 16:57:43  velktron
-//Addressed generics warnings.
-//
-//Revision 1.10.2.1  2012/09/20 14:18:06  velktron
-//NEW and ENHANCED dual-function main!!!
-//
-//Revision 1.10  2011/10/24 02:11:27  velktron
-//Stream compliancy
-//
-//Revision 1.9  2011/06/01 00:08:35  velktron
-//Downsized to essentiality.
-//
-//Revision 1.8  2011/05/31 16:27:14  velktron
-//Resolution not handled here anymore.
-//
-//Revision 1.7  2011/05/30 15:51:32  velktron
-//*** empty log message ***
-//
-//Revision 1.6  2011/05/26 13:39:26  velktron
-//Now using ICommandLineManager
-//
-//Revision 1.5  2011/05/25 17:56:30  velktron
-//just played a bit with resolutions.
-//
-//Revision 1.4  2011/05/23 16:59:59  velktron
-//Migrated to VideoScaleInfo.
-//
-//Revision 1.3  2010/09/25 17:37:13  velktron
-//Lots of changes.
-//
-//The most important is the creation of the AWTDoom frame handling I/O.
-//
-//Revision 1.2  2010/09/23 07:31:11  velktron
-//fuck
-//
-//Revision 1.1  2010/09/22 16:40:02  velktron
-//MASSIVE changes in the status passing model.
-//DoomMain and DoomGame unified.
-//Doomstat merged into DoomMain (now status and game functions are one).
-//
-//Most of DoomMain implemented. Possible to attempt a "classic type" start but will stop when reading sprites.
-//
 //
 //DESCRIPTION:
 //Main program, simply calls D_DoomMain high level loop.
@@ -79,14 +35,16 @@ import doom.ICommandLineManager;
 
 
 public class Main {
-    static final String rcsid = "$Id: Main.java,v 1.11 2012/09/24 17:16:22 velktron Exp $";
+    static final String rcsid = "$Id: Main.java,v 1.12 2012/09/25 16:35:38 velktron Exp $";
 
+    public static BppMode bpp;
+    
     public static void main(String[] argv) throws IOException{
 
     	//  First, get the command line parameters.
             ICommandLineManager CM=new CommandLine(argv);
         
-          BppMode bpp=BppMode.Indexed;
+          bpp= BppMode.Indexed;
             
           if (CM.CheckParmBool("-hicolor")) bpp=BppMode.HiColor;
               else
@@ -94,6 +52,9 @@ public class Main {
           
           // Here he create DOOM
           DoomMain<?, ?> DM=null;
+          // Create a dummy. This will force static init to run.
+          //LightsAndColors LAC=new LightsAndColors();
+          
           
           switch(bpp){
           case Indexed:
