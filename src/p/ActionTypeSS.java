db72cf0a025cadefea2e3d6de19cfb15cc59d9d7
velktron <velktron>
2012-11-22 16:04:51 +0000

    New generic action type

--- src/p/ActionTypeSS.java
+++ src/p/ActionTypeSS.java
@@ -1,5 +1,16 @@
 package p;
 
-public interface ActionTypeSS{
-	public void invoke (Object a);
+import doom.thinker_t;
+
+/** Generic single-thinker argument action function. 
+ *  Useful for handling stuff such as sector actions, doors, etc.
+ *  or special thinker objects that don't qualify as mobj_t's.
+ * 
+ * @author velktron
+ *
+ * @param <T>
+ */
+
+public interface ActionTypeSS<T extends thinker_t>{
+	public void invoke (T a);
 }
