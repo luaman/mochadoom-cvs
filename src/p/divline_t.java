6088d45934829486d7248a2212ba499edbdc7215
velktron <velktron>
2013-06-04 11:23:04 +0000

    Using boom functions.

--- src/p/divline_t.java
+++ src/p/divline_t.java
@@ -2,7 +2,7 @@ package p;
 
 import static m.fixed_t.*;
 import rr.line_t;
-
+import static utils.C2JUtils.eval;
 //
 // P_MAPUTL
 //
@@ -26,6 +26,25 @@ public class divline_t {
      int   y
      )
      {
+    	 
+    	 
+    	 // Using Killough's version.
+    	  return
+    	    (dx==0) ? x <= this.x ? dy > 0 : dy < 0 :
+    	    (dy==0) ? y <= this.y ? dx < 0 : dx > 0 :
+    	    (dy^dx^(x -= this.x)^(y -= this.y)) < 0 ? (dy^x) < 0 :
+    	    FixedMul(y>>8, this.dx>>8) >= FixedMul(this.dy>>8, x>>8);
+    	    /*
+    	    int PUREFUNC P_PointOnDivlineSide(fixed_t x, fixed_t y, const divline_t *line)
+    	    {
+    	      return
+    	        !line->dx ? x <= line->x ? line->dy > 0 : line->dy < 0 :
+    	        !line->dy ? y <= line->y ? line->dx < 0 : line->dx > 0 :
+    	        (line->dy^line->dx^(x -= line->x)^(y -= line->y)) < 0 ? (line->dy^x) < 0 :
+    	        FixedMul(y>>8, line->dx>>8) >= FixedMul(line->dy>>8, x>>8);
+    	    }*/
+    	    
+    /*
       int dx;
       int dy;
       int left;
@@ -63,6 +82,7 @@ public class divline_t {
       if (right < left)
       return false;       // front side
       return true;           // back side
+      */
      }
 
 
@@ -103,46 +123,61 @@ public class divline_t {
  	( int	x,
  	  int	y)
  	{
- 	    int	dx; // fixed_t
- 	    int	dy;
- 	    int	left;
- 	    int	right;
+ 	    
+ 	   int left,right;
+ 	    // Boom-style code. Da fack.
+ 	   // [Maes:] it is MUCH more corrent than the linuxdoom one, for whatever reason.
+ 	    
+ 	   return
+ 	  (this.dx==0) ? x == this.x ? 2 : x <= this.x ? eval(this.dy > 0) : eval(this.dy < 0) :
+ 	  (this.dy==0) ? (olddemo ? x : y) == this.y ? 2 : y <= this.y ? eval(this.dx < 0) : eval(this.dx > 0) :
+ 	  (this.dy==0) ? y == this.y ? 2 : y <= this.y ? eval(this.dx < 0) : eval(this.dx > 0) :
+ 	  (right = ((y - this.y) >> FRACBITS) * (this.dx >> FRACBITS)) <
+ 	  (left  = ((x - this.x) >> FRACBITS) * (this.dy >> FRACBITS)) ? 0 :
+ 	  right == left ? 2 : 1;
+ 	  
+ 	  /*  
+ 	    
+ 	    int	left,right,dx,dy;
 
  	    if (this.dx==0)
  	    {
- 		if (x==this.x)
- 		    return 2;
- 		
- 		if (x <= this.x)
- 		    return (this.dy > 0)?1:0;
+ 	    if (x==this.x)
+ 	        return 2;
+ 	    
+ 	    if (x <= this.x)
+ 	        return eval(this.dy > 0);
 
- 		return (this.dy < 0)?1:0;
+ 	    return eval(this.y < 0);
  	    }
  	    
  	    if (this.dy==0)
  	    {
- 		if (x==this.y)
- 		    return 2;
+ 	    if (x==this.y)
+ 	        return 2;
 
- 		if (y <= this.y)
- 		    return (this.dx < 0)?1:0;
+ 	    if (y <= this.y)
+ 	        return eval(this.dx < 0);
 
- 		return (this.dx > 0)?1:0;
+ 	    return eval(this.dx > 0);
  	    }
- 		
+ 	    
  	    dx = (x - this.x);
  	    dy = (y - this.y);
 
  	    left =  (this.dy>>FRACBITS) * (dx>>FRACBITS);
  	    right = (dy>>FRACBITS) * (this.dx>>FRACBITS);
- 		
+ 	    
  	    if (right < left)
- 		return 0;	// front side
+ 	    return 0;   // front side
  	    
  	    if (left == right)
- 		return 2;
- 	    return 1;		// back side
+ 	    return 2;
+ 	    return 1;       // back side
+ 	    */
  	}
+ 	
+ 	private static final boolean olddemo = true;
      
      
  }
